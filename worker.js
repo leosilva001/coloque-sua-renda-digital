74c97d94dcdc34afc3e3bef87024a45d6331d903
leosilva001 <leonardodominguesmacedo@gmail.com>
2026-09-15 16:04:03 -0300

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,11 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/health") {
+      return new Response("OK - Sua Renda no Digital");
+    }
+
+    return new Response("Sua Renda no Digital");
+  }
+};
